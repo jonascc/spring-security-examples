4e3015e35f69262a10813f8ee1c3ef46145fe531
Jonas Carmo <jonasccarmo@gmail.com>
2026-03-31 20:57:09 -0300

    First commit

---
+++ src/main/java/com/jonas/spring_security_examples/SpringSecurityExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.jonas.spring_security_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonas/spring_security_examples/SpringSecurityExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.spring_security_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
