826592d093ff93a3e1fd7ee8e8533bf76cdf6ce6
Jonas Carmo <jonasccarmo@gmail.com>
2026-04-03 20:16:45 -0300

    3.3.3 Implementing the UserDetailsManager contract (JdbcUserDetailsManager)

--- src/main/java/com/jonas/spring_security_examples/config/ProjectConfig.java
+++ src/main/java/com/jonas/spring_security_examples/config/ProjectConfig.java
@@ -1,24 +1,32 @@
 package com.jonas.spring_security_examples.config;
 
-import com.jonas.spring_security_examples.services.InMemoryUserDetailsService;
-import com.jonas.spring_security_examples.users.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig {
 
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    //The JdbcUserDetailsManager implementation expects the column's names to be username and password; in this case, use bean above. However, it is possible to override the queries for JdbcUserDetailsManager
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails u = new User("john", "12345", "read");
-        List<UserDetails> users = List.of(u);
-        return new InMemoryUserDetailsService(users);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
+        String authsByUserQuery = "select username, authority from authorities where username = ?";
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
     }
 
     @Bean
