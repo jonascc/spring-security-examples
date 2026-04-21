aba29af887a6ad1b61b99a3363498c2105f2f002
Jonas Carmo <jonasccarmo@gmail.com>
2026-04-21 18:09:58 -0300

    4.2.2 Encrypting and decrypting secrets using encryptors

--- src/main/java/com/jonas/spring_security_examples/SpringSecurityExamplesApplication.java
+++ src/main/java/com/jonas/spring_security_examples/SpringSecurityExamplesApplication.java
@@ -1,13 +1,37 @@
 package com.jonas.spring_security_examples;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.encrypt.BytesEncryptor;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 
 @SpringBootApplication
 public class SpringSecurityExamplesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityExamplesApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String salt = KeyGenerators.string().generateKey();
+			String password = "secret";
+			String valueToEncrypt = "HELLO";
+
+			BytesEncryptor e = Encryptors.stronger(password, salt);
+			byte[] encrypted = e.encrypt(valueToEncrypt.getBytes());
+			String encryptedString = new String(encrypted);
+			String decrypted = new String(e.decrypt(encrypted));
+
+			TextEncryptor te = Encryptors.text(password, salt);
+			String encryptedText = te.encrypt(valueToEncrypt);
+			String decryptedText = te.decrypt(encryptedText);
+		};
 	}
 
 }
