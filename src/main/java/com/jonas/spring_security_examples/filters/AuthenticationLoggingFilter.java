60776296d8bb0bc4b4398a55c3180a67cec56084
Jonas Carmo <jonasccarmo@gmail.com>
2026-06-20 11:45:54 -0300

    5.5 Filter implementation provided by Spring Security

--- src/main/java/com/jonas/spring_security_examples/filters/AuthenticationLoggingFilter.java
+++ src/main/java/com/jonas/spring_security_examples/filters/AuthenticationLoggingFilter.java
@@ -3,16 +3,18 @@ package com.jonas.spring_security_examples.filters;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.logging.Logger;
 
 
-public class AuthenticationLoggingFilter implements Filter {
+public class AuthenticationLoggingFilter extends OncePerRequestFilter {
 
     private final Logger logger = Logger.getLogger(AuthenticationLoggingFilter.class.getName());
 
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    @Override
+    protected void doFilterInternal(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         var httpRequest = (HttpServletRequest) servletRequest;
         String requestId = httpRequest.getHeader("Request-Id");
 
